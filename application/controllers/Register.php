d6baa34d243746cb4bb648029d0708442306a19e
Tung Nguyen <tung18tht@gmail.com>
2016-10-27 23:15:54 +0700

    Add form validation on Register controller

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -5,5 +5,12 @@ class Register extends MY_Controller {
     public function index() {
         $this->load->helper('form');
         $this->render('register/index_view');
+
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('first_name', 'First name','trim|required');
+        $this->form_validation->set_rules('last_name', 'Last name','trim|required');
+        $this->form_validation->set_rules('email','Email','trim|valid_email|required|is_unique[users.email]');
+        $this->form_validation->set_rules('password','Password','min_length[8]|max_length[20]|required');
+        $this->form_validation->set_rules('confirm_password','Confirm password','matches[password]|required');
     }
 }
