66164188e2075a456522cea0e50eafdd0a0a7cf6
Tung Nguyen <tung18tht@gmail.com>
2016-10-27 22:47:00 +0700

    Make dashboard index view

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -3,6 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  
 class Dashboard extends Auth_Controller {
     public function index() {
-        echo 'Hello from the dashboard';
+        $this->render('dashboard/index_view');
     }
 }
