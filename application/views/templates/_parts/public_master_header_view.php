ad772fb0bca76205f0b940a80c436bc69e4d3be8
Tung Nguyen <tung18tht@gmail.com>
2016-10-31 23:40:29 +0700

    Properly display page title

--- application/views/templates/_parts/public_master_header_view.php
+++ application/views/templates/_parts/public_master_header_view.php
@@ -5,8 +5,10 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title><?php echo $page_title;?></title>
-    <meta name="description" value="<?php echo $page_description;?>" />
+    <title><?php
+        echo 'ICTLab';
+        echo ($page_title=='') ? FALSE : (' | ' . $page_title);
+    ?></title>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <?php echo $before_closing_head;?>
 </head>
