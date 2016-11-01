3aa01ef9e0ba6458f9f032e48b9439bbc343a0dc
Tung Nguyen <tung18tht@gmail.com>
2016-11-01 16:20:32 +0700

    Done layout register page

--- application/views/register/index_view.php
+++ application/views/register/index_view.php
@@ -1,9 +1,8 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
 <div class="container">
-    <?php
+    <!-- <?php
     echo isset($_SESSION['auth_message']) ? $_SESSION['auth_message'] : FALSE;
     ?>
-    <h1>Register</h1>
     <?php
     echo form_open();
     echo form_label('First name:','first_name').'<br />';
@@ -23,5 +22,79 @@
     echo form_password('confirm_password').'<br /><br />';
     echo form_submit('register','Register');
     echo form_close();
-    ?>
+    ?> -->
+
+    <div id="signupbox" class="col-md-8 col-md-offset-2">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <div class="panel-title">Sign Up</div>
+            </div>  
+            <div class="panel-body" >
+                <form id="signupform" class="form-horizontal" method="post" accept-charset="utf-8"
+                    action="<?php echo base_url("index.php/register"); ?>"
+                >
+                    
+                    <!-- <?php
+                        if (form_error('first_name')==='') {
+                            echo 'lmao';
+                        } else {
+                            echo 'lol';
+                        }
+                        
+                        // <div id="signupalert" style="display:none" class="alert alert-danger">
+                        //     <p>Error:</p>
+                        //     <span></span>
+                        // </div>
+                    ?> -->
+                                
+                    <div class="form-group">
+                        <label for="first_name" class="col-md-3 control-label">First Name</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" name="first_name" placeholder="First Name">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="last_name" class="col-md-3 control-label">Last Name</label>
+                        <div class="col-md-9">
+                            <input type="text" class="form-control" name="last_name" placeholder="Last Name">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email" class="col-md-3 control-label">Email</label>
+                        <div class="col-md-9">
+                            <input type="email" class="form-control" name="email" placeholder="Email Address">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password" class="col-md-3 control-label">Password</label>
+                        <div class="col-md-9">
+                            <input type="password" class="form-control" name="password" placeholder="Password (8~20 characters)">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="confirm_password" class="col-md-3 control-label">Confirm Password</label>
+                        <div class="col-md-9">
+                            <input type="password" class="form-control" name="confirm_password" placeholder="Re-enter Password">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-md-offset-3 col-md-9">
+                            <input type="submit" name="register" class="col-md-4 btn btn-info" value="Register">
+                        </div>
+                    </div>
+                </form>
+                    
+                <div id="footersignin">
+                    <div class="text-center">
+                        Already a member? <?php echo anchor('user/login', 'Login here');?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
