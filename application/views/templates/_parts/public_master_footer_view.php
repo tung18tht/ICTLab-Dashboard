5f5983200a59738acef5e8a3705d214b7a4a089a
Tung Nguyen <tung18tht@gmail.com>
2016-11-02 11:54:14 +0700

    Add script.js

--- application/views/templates/_parts/public_master_footer_view.php
+++ application/views/templates/_parts/public_master_footer_view.php
@@ -1,6 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
 <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-3.1.1.js"); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url("assets/bootstrap/js/bootstrap.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/js/script.js"); ?>"></script>
 <?php echo $before_closing_body;?>
 </body>
 </html>
