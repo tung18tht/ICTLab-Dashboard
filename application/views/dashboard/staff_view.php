8e72bef191dd2c38ad849b5b62fcd2ede39f01a0
Tung Nguyen <tung18tht@gmail.com>
2016-11-06 15:02:53 +0700

    Add view for Staff page

--- application/views/dashboard/staff_view.php
+++ application/views/dashboard/staff_view.php
@@ -1,47 +1,21 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
 <div class="container">
     <div class="row">
-        <div class="col-lg-12">
-            <h2 class="page-header">Our Team</h2>
-        </div>
-        <div class="col-lg-4 col-sm-6 text-center">
-            <img class="img-circle img-responsive img-center img-thumbnail" src="http://placehold.it/200x200" alt="">
-            <h3>John Smith
-                <small>Job Title</small>
-            </h3>
-        </div>
-        <div class="col-lg-4 col-sm-6 text-center">
-            <img class="img-circle img-responsive img-center img-thumbnail" src="http://placehold.it/200x200" alt="">
-            <h3>John Smith
-                <small>Job Title</small>
-            </h3>
-        </div>
-        <div class="col-lg-4 col-sm-6 text-center">
-            <img class="img-circle img-responsive img-center img-thumbnail" src="http://placehold.it/200x200" alt="">
-            <h3>John Smith
-                <small>Job Title</small>
-            </h3>
-        </div>
-        <div class="col-lg-4 col-sm-6 text-center">
-            <img class="img-circle img-responsive img-center img-thumbnail" src="http://placehold.it/200x200" alt="">
-            <h3>John Smith
-                <small>Job Title</small>
-            </h3>
-        </div>
-        <div class="col-lg-12">
-            <h2 class="page-header">Our Team</h2>
-        </div>
-        <div class="col-lg-4 col-sm-6 text-center">
-            <img class="img-circle img-responsive img-center img-thumbnail" src="http://placehold.it/200x200" alt="">
-            <h3>John Smith
-                <small>Job Title</small>
-            </h3>
-        </div>
-        <div class="col-lg-4 col-sm-6 text-center">
-            <img class="img-circle img-responsive img-center img-thumbnail" src="http://placehold.it/200x200" alt="">
-            <h3>John Smith
-                <small>Job Title</small>
-            </h3>
-        </div>
+        <?php foreach($groups as $group) { ?>
+            <div class="col-lg-12">
+                <h2 class="page-header"><?php echo $group['group_name']; ?></h2>
+            </div>
+
+            <?php foreach($group['staffs'] as $staff) { ?>
+                <div class="col-lg-4 col-sm-6 text-center">
+                    <img class="img-circle img-responsive img-center img-thumbnail" alt="avatar"
+                        src="<?php echo base_url("assets/avatar/".$staff['avatar'].".png"); ?>">
+                    <h3>
+                        <?php echo $staff['first_name'] . " " . $staff['last_name']; ?>
+                        <small><?php echo $staff['title'] ?></small>
+                    </h3>
+                </div>
+            <?php } ?>
+        <?php } ?>
     </div>
 </div>
