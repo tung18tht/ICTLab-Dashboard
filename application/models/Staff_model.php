70eadb77be6b05950848fc91a2c15191aba67183
Tung Nguyen <tung18tht@gmail.com>
2016-11-06 16:38:29 +0700

    Done model for staffs

--- application/models/Staff_model.php
+++ application/models/Staff_model.php
@@ -1,8 +1,34 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Staff_Model extends Profile_model {
+class Staff_Model extends CI_Model {
     function __construct() {
         parent::__construct();
+    }
+
+    public function get_staffs_in_position_groups() {
+        $position_list = $this->get_unique_position_list();
+
+        $position_groups = array();
+        foreach ($position_list as $key=>$position) {
+            $this->db->select('*');
+            $this->db->from('profile');
+            $this->db->join('users', 'user_id = id');
+            $this->db->where('position', $position);
+            $staffs = $this->db->get()->result_array();
+
+            $position_groups[$key] = array('name' => $position, 'staffs' => $staffs);
+        }
+
+        return $position_groups;
+    }
+
+    public function get_unique_position_list() {
+        $profile_table = $this->db->get('profile')->result_array();
+        $positions = array();
+        foreach ($profile_table as $profile) {
+            array_push($positions, $profile['position']);
+        }
+        return array_unique($positions);
     }
 }
--- application/views/dashboard/staff_view.php
+++ application/views/dashboard/staff_view.php
@@ -3,13 +3,18 @@
     <div class="row">
         <?php foreach($position_groups as $group) { ?>
             <div class="col-lg-12">
-                <h2 class="page-header"><?php echo $group['group_name']; ?></h2>
+                <h2 class="page-header"><?php echo $group['name']; ?></h2>
             </div>
 
             <?php foreach($group['staffs'] as $staff) { ?>
                 <div class="col-lg-4 col-sm-6 text-center">
                     <img class="img-circle img-responsive img-center img-thumbnail" alt="avatar"
-                        src="<?php echo base_url("assets/avatar/".$staff['avatar'].".png"); ?>">
+                        src="
+                        <?php
+                            echo base_url("assets/avatar/");
+                            echo ($staff['has_avatar']==0) ? 0 : $staff['user_id'];
+                            echo ".png"; 
+                        ?>">
                     <h3>
                         <?php echo $staff['first_name'] . " " . $staff['last_name']; ?>
                         <small><?php echo $staff['title'] ?></small>
