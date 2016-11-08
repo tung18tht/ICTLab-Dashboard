ce67fc995f5e0412bf804951d56b650f1bcd0582
Tung Nguyen <tung18tht@gmail.com>
2016-11-09 00:25:23 +0700

    Done view controller for news, events

--- application/controllers/News_events.php
+++ application/controllers/News_events.php
@@ -8,5 +8,35 @@ class News_events extends Auth_Controller {
         $this->load->model('news_events_model');
     }
 
-    public function
+    public function news() {
+        $this->data['page_title'] = 'News';
+
+        $this->data['news'] = $this->news_events_model->get_news();
+
+        $this->render('news_events/news_view');
+    }
+
+    public function view_news($id) {
+        $this->data['page_title'] = 'News';
+
+        $this->data['news'] = $this->news_events_model->get_news_row($id);
+
+        $this->render('news_events/view_news_view');
+    }
+
+    public function events() {
+        $this->data['page_title'] = 'Events';
+
+        $this->data['events'] = $this->news_events_model->get_events();
+
+        $this->render('news_events/events_view');
+    }
+
+    public function view_event($id) {
+        $this->data['page_title'] = 'Event';
+
+        $this->data['event'] = $this->news_events_model->get_event_row($id);
+
+        $this->render('news_events/view_event_view');
+    }
 }
