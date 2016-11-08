af42b5a53d91b90c3ac43e72e67fe2310ad13c5f
Tung Nguyen <tung18tht@gmail.com>
2016-11-08 23:47:06 +0700

    Update information controller for projects

--- application/controllers/Information.php
+++ application/controllers/Information.php
@@ -30,6 +30,18 @@ class Information extends Auth_Controller {
         $this->render('information/research_topic_view');
     }
 
+    public function swarms() {
+        $this->data['page_title'] = "SWARMS";
+        $this->data['content'] = $this->information_model->get_swarms()['content'];
+        $this->render('information/swarms_view');
+    }
+
+    public function archives() {
+        $this->data['page_title'] = "ARCHIVES";
+        $this->data['content'] = $this->information_model->get_archives()['content'];
+        $this->render('information/archives_view');
+    }
+
     public function edit_about() {
         if (!($this->ion_auth->is_admin())) {
             redirect("information/about");
@@ -93,6 +105,50 @@ class Information extends Auth_Controller {
             $_SESSION['auth_message'] = 'Research Topics updated.';
             $this->session->mark_as_flash('auth_message');
             redirect("information/research_topic");
+        }
+    }
+
+    public function edit_swarms() {
+        if (!($this->ion_auth->is_admin())) {
+            redirect("information/swarms");
+        }
+
+        $this->data['page_title'] = "Edit SWARMS";
+
+        $this->data['content'] = $this->information_model->get_swarms()['content'];
+        
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('content', 'Content', 'trim|required');
+        if ($this->form_validation->run() === FALSE) {
+            $this->load->helper('form');
+            $this->render('information/edit_swarms_view');
+        } else {
+            $this->information_model->update_swarms($this->input->post('content'));
+            $_SESSION['auth_message'] = 'SWARMS updated.';
+            $this->session->mark_as_flash('auth_message');
+            redirect("information/swarms");
+        }
+    }
+
+    public function edit_archives() {
+        if (!($this->ion_auth->is_admin())) {
+            redirect("information/archives");
+        }
+
+        $this->data['page_title'] = "Edit ARCHIVES";
+
+        $this->data['content'] = $this->information_model->get_archives()['content'];
+        
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('content', 'Content', 'trim|required');
+        if ($this->form_validation->run() === FALSE) {
+            $this->load->helper('form');
+            $this->render('information/edit_archives_view');
+        } else {
+            $this->information_model->update_archives($this->input->post('content'));
+            $_SESSION['auth_message'] = 'ARCHIVES updated.';
+            $this->session->mark_as_flash('auth_message');
+            redirect("information/archives");
         }
     }
 }
